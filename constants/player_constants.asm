pret/pokered

Define player and rival names once, to be used in two places (#545)

constants/player_constants.asm
@@ -1,0 +1,21 @@
+DEF NUM_PLAYER_NAMES EQU 3
+
+IF DEF(_RED)
+DEF PLAYERNAME1 EQUS "RED"
+DEF PLAYERNAME2 EQUS "ASH"
+DEF PLAYERNAME3 EQUS "JACK"
+
+DEF RIVALNAME1 EQUS "BLUE"
+DEF RIVALNAME2 EQUS "GARY"
+DEF RIVALNAME3 EQUS "JOHN"
+ENDC
+
+IF DEF(_BLUE)
+DEF PLAYERNAME1 EQUS "BLUE"
+DEF PLAYERNAME2 EQUS "GARY"
+DEF PLAYERNAME3 EQUS "JOHN"
+
+DEF RIVALNAME1 EQUS "RED"
+DEF RIVALNAME2 EQUS "ASH"
+DEF RIVALNAME3 EQUS "JACK"
+ENDC

kagnusdev/pokered-fr

constants/player_constants.asm
@@ -1,0 +1,21 @@
+DEF NUM_PLAYER_NAMES EQU 3
+
+IF DEF(_RED)
+DEF PLAYERNAME1 EQUS "RED"
+DEF PLAYERNAME2 EQUS "SACHA"
+DEF PLAYERNAME3 EQUS "PAUL"
+
+DEF RIVALNAME1 EQUS "BLUE"
+DEF RIVALNAME2 EQUS "REGIS"
+DEF RIVALNAME3 EQUS "JEAN"
+ENDC
+
+IF DEF(_BLUE)
+DEF PLAYERNAME1 EQUS "BLUE"
+DEF PLAYERNAME2 EQUS "REGIS"
+DEF PLAYERNAME3 EQUS "JEAN"
+
+DEF RIVALNAME1 EQUS "RED"
+DEF RIVALNAME2 EQUS "SACHA"
+DEF RIVALNAME3 EQUS "PAUL"
+ENDC